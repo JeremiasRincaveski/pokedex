96bff66fd1782014e9848161e27de21cb79ee73a
jeremiasRincaveski <jrincaveski@gmail.com>
2023-08-26 22:27:55 -0300

    esqueleto

---
+++ js/server.js
@@ -0,0 +1,45 @@
+const ul = document.getElementById("pokemons");
+
+const criaPokemon = pokemon => {
+  const {name : nome, id, types: tipos, sprites: {back_default : img}} = pokemon
+  const li = document.createElement(`li`);
+  li.className = `pokemon`
+  li.innerHTML = `
+    <div class="wrapper">
+      <h2 class="pokemonNome">${nome}</h2>
+      <span>#${id}</span>
+    </div>
+
+    <div class="wrapper">
+      <ul class="pokemonAtributos">
+      </ul>
+
+      <img class="pokemonImg" src="${img}" alt="pokemon ${nome}">
+    </div>
+  `;
+
+  const atributos = li.querySelector(`.pokemonAtributos`);
+
+  tipos.forEach(tipos => {
+    const li = document.createElement(`li`);
+    li.innerText = tipos.type.name;
+    atributos.appendChild(li);
+  })
+
+  ul.appendChild(li);
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`)
+  .then(data => data.json())
+  .then(data => data.results)
+  .then(data => {
+    const promesas = []
+    data.forEach(pokemon => {
+      promesas.push(fetch(pokemon.url).then(data => data.json()))
+    })
+
+    Promise.all(promesas)
+      .then(responses => {
+        responses.forEach(criaPokemon)
+      })
+  })
